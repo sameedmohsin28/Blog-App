4b8f7c74201e6540eb73e2843e809646b782f17b
sameedmohsin28 <sameedmohsin22@hotmail.com>
2023-08-02 15:29:34 +0500

    n+1 solution

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   def index
-    @user = User.find_by(id: params[:user_id])
-    @posts = Post.where(author_id: params[:user_id])
+    @user = User.find(params[:user_id])
+    @posts = @user.posts.includes(:comments, :likes)
   end
 
   def new
